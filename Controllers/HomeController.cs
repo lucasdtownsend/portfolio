baec84ca6fb46985fc97a2ec1be5903be67f5d7b
Lucas Townsend <lucasdtownsend@gmail.com>
2015-11-19 20:08:14 -0500

    All content in. Needs to be prettier.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TownsendPortfolio.Models;
+
+namespace TownsendPortfolio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ldtPortfolioEntities1 db = new ldtPortfolioEntities1();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Projects.ToList());
+        }
+    }
+}
